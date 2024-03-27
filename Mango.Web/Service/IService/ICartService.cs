e98c4e1ea2f15a57772331923fc4852e3e9621f7
abhijitkichambare39 <abhijitkichambare39@gmail.com>
2024-03-28 03:22:46 +0530

    Configurations & Models Creation

---
+++ Mango.Web/Service/IService/ICartService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Models.Dto.Cart;
+using Mango.Web.Models.Dto.Coupon;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICartService
+    {
+        Task<ResponseDto?> GetCartByUserIdAsync(string userId);
+        Task<ResponseDto?> UpsertCartAsync(CartDto cartDto);
+        Task<ResponseDto?> RemoveFromCartAsync(int cartDetailsId);
+        Task<ResponseDto?> ApplyCouponAsync(CartDto cartDto);
+
+    }
+}
