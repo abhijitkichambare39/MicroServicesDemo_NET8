34d90cee6146cb841a97f3fb0da44701cbc85812
abhijitkichambare39 <abhijitkichambare39@gmail.com>
2024-04-05 03:36:27 +0530

    Configurations & Models

---
+++ Mango.Services.OrderAPI/Service/Implementation/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using Mango.Services.OrderAPI.Models.Dto;
+using Mango.Services.OrderAPI.Service.IService;
+using Newtonsoft.Json;
+
+namespace Mango.Services.OrderAPI.Service.Implementation
+{
+    public class ProductService : IProductService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ProductService(IHttpClientFactory clientFactory)
+        {
+            _httpClientFactory = clientFactory;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            var client = _httpClientFactory.CreateClient("Product");
+            var getProduct = await client.GetAsync($"/api/product");
+            var apicontent = await getProduct.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject<ResponseDto>(apicontent);
+            if (response.IsSuccess)
+            {
+                return JsonConvert.DeserializeObject<IEnumerable<ProductDto>>(Convert.ToString(response.Result));
+            }
+            return new List<ProductDto>();
+
+        }
+    }
+}
---
+++ Mango.Services.OrderAPI/Utility/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+
+namespace Mango.Services.OrderAPI.Utility
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMapper()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                //config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                //config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+
+
+    }
+}
